bdaf524229056dc07e1729b3ae27f836497412b2
John McIver <john.mciver.iii@gmail.com>
2024-06-25 18:40:06 -0600

    [fftw] Fix autoconf clang support to enable avx512 instructions

--- pts/fftw-1.2.0/install.sh
+++ pts/fftw-1.2.0/install.sh
@@ -5,6 +5,28 @@ rm -rf fftw-mr
 rm -rf fftw-stock
 
 mv fftw-3.3.6-pl2 fftw-stock
+pushd fftw-stock > /dev/null 2>&1
+cat <<EOF > configure.ac.patch
+--- configure.ac	2017-01-27 21:08:13.000000000 +0000
++++ configure.ac.new	2024-06-25 18:28:06.901371406 +0000
+@@ -457,6 +457,12 @@
+             AX_CHECK_COMPILER_FLAGS(-mfma, [AVX2_CFLAGS="\$AVX2_CFLAGS -mfma"])
+         fi
+ 
++        # AVX512
++        if test "\$have_avx512" = "yes" -a "x\$AVX512_CFLAGS" = x; then
++            AX_CHECK_COMPILER_FLAGS(-mavx512f, [AVX512_CFLAGS="-mavx512f"],
++                [AC_MSG_ERROR([Need a version of clang with -mavx512f])])
++        fi
++
+         if test "\$have_vsx" = "yes" -a "x\$VSX_CFLAGS" = x; then
+             # clang appears to need both -mvsx and -maltivec for VSX
+             AX_CHECK_COMPILER_FLAGS(-maltivec, [VSX_CFLAGS="-maltivec"],
+
+EOF
+patch < configure.ac.patch
+autoconf
+popd > /dev/null 2>&1
 cp -a fftw-stock fftw-mr
 
 AVX_TUNING=""
