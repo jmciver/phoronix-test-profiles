ad1e3f5a4ba54104d42fc8364664f2bc759a6f20
John McIver <john.mciver.iii@gmail.com>
2025-01-09 19:20:03 -0700

    [botan] Add alivecc compile compatibility

--- pts/botan-1.6.0/install.sh
+++ pts/botan-1.6.0/install.sh
@@ -3,10 +3,15 @@
 tar -xf Botan-2.17.3.tar.xz
 
 cd Botan-2.17.3
-python3 ./configure.py
 if [[ ! -z "$ALIVECC_PARALLEL_FIFO" ]]; then
+    sed -E \
+        -e 's/^macro_name CLANG/macro_name ALIVECC/' \
+        -e 's/^binary_name clang/binary_name alivecc/' \
+        src/build-data/cc/clang.txt > src/build-data/cc/alivecc.txt
+    python3 ./configure.py --cc=alivecc --cc-bin=/llvm/alive2/build/release/alivecc
     "$ALIVE2_JOB_SERVER_PATH" "-j${ALIVE2_JOB_SERVER_THREADS}" make "-j${NUM_CPU_CORES}"
 else
+    python3 ./configure.py
     make "-j${NUM_CPU_CORES}"
 fi
 echo $? > ~/install-exit-status
