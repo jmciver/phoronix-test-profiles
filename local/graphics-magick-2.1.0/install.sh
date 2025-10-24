0ad22b8a61ab692de2292b74748212cdfe3853d5
John McIver <john.mciver.iii@gmail.com>
2025-10-23 22:19:02 -0400

    [graphics-magick][local] Add v2.1.0 with bitfield statistics support

---
+++ local/graphics-magick-2.1.0/install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+tar -xjf GraphicsMagick-1.3.38.tar.bz2
+unzip -o sample-photo-6000x4000-1.zip
+
+mkdir $HOME/gm_
+cd GraphicsMagick-1.3.38/
+
+MAKE_PROGRAM=make
+if [ $OS_TYPE = "BSD" ]; then
+   MAKE_PROGRAM=gmake
+fi
+
+if [[ ! -z "$ALIVECC_PARALLEL_FIFO" ]]; then
+    LDFLAGS="-L$HOME/gm_/lib" CPPFLAGS="-I$HOME/gm_/include" ./configure --without-perl --prefix=$HOME/gm_ --without-png --disable-openmp > /dev/null
+    "$ALIVE2_JOB_SERVER_PATH" "-j${ALIVE2_JOB_SERVER_THREADS}" "$MAKE_PROGRAM" "-j${NUM_CPU_CORES}" magick/libGraphicsMagick.la
+elif [[ ! -z "$CLANG_PLUGIN_STATISTICS" ]]; then
+    LDFLAGS="-L$HOME/gm_/lib" \
+    CPPFLAGS="-I$HOME/gm_/include $CLANG_PLUGIN_STATISTICS" \
+    ./configure --without-perl --prefix=/work/jmciver/u2p/gm_ --without-png --disable-openmp && \
+        make |& tee run-build.log && \
+        sed -i 's_^\./__g' run-build.log
+else
+    LDFLAGS="-L$HOME/gm_/lib" CPPFLAGS="-I$HOME/gm_/include" ./configure --without-perl --prefix=$HOME/gm_ --without-png --disable-openmp > /dev/null
+    "$MAKE_PROGRAM" "-j${NUM_CPU_CORES}"
+fi
+
+echo $? > ~/install-exit-status
+"$MAKE_PROGRAM" install
+
+cd ~
+if [[ -z "$ALIVECC_PARALLEL_FIFO" && -z "$CLANG_PLUGIN_STATISTICS" ]]; then
+    rm -rf GraphicsMagick-1.3.38/
+    rm -rf gm_/share/doc/GraphicsMagick/
+    rm -rf gm_/share/man/
+fi
+
+./gm_/bin/gm convert sample-photo-6000x4000.JPG input.mpc
+
+TASKSET="nice -n -20 taskset -c 1"
+
+echo "#!/bin/sh
+$TASKSET ./gm_/bin/gm benchmark -duration 60 convert input.mpc \$@ null: > \$LOG_FILE 2>&1
+echo \$? > ~/test-exit-status" > graphics-magick
+chmod +x graphics-magick
