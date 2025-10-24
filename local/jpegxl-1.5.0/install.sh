ca8c17fd1210d86481ac9893dd03c29e1fcaa571
John McIver <john.mciver.iii@gmail.com>
2025-10-23 22:19:35 -0400

    [jpegxl][local] Add v1.5.0 with bitfield statistics support

---
+++ local/jpegxl-1.5.0/install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+tar -xf libjxl-0.7.0.tar.gz
+tar -xf png-samples-1.tar.xz
+unzip -o sample-photo-6000x4000-1.zip
+cd libjxl-0.7.0
+./deps.sh
+mkdir build
+cd build
+if [[ ! -z "$ALIVECC_PARALLEL_FIFO" ]]; then
+    cmake \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DBUILD_TESTING=OFF \
+        -DJPEGXL_FORCE_SYSTEM_BROTLI=ON .. && \
+        "$ALIVE2_JOB_SERVER_PATH" "-j${ALIVE2_JOB_SERVER_THREADS}" cmake --build . -- "-j${NUM_CPU_CORES}"
+elif [[ ! -z "$CLANG_PLUGIN_STATISTICS" ]]; then
+    cmake \
+        -GNinja \
+        -DCMAKE_C_FLAGS_RELEASE="$CLANG_PLUGIN_STATISTICS" \
+        -DCMAKE_CXX_FLAGS_RELEASE="$CLANG_PLUGIN_STATISTICS" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DBUILD_TESTING=OFF \
+        -DJPEGXL_FORCE_SYSTEM_BROTLI=ON .. && \
+        ninja -j${NUM_CPU_CORES} |& tee run-build.log
+else
+    cmake \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DBUILD_TESTING=OFF \
+        -DJPEGXL_FORCE_SYSTEM_BROTLI=ON .. && \
+        cmake --build . -- "-j${NUM_CPU_CORES}"
+fi
+echo $? > ~/install-exit-status
+
+TASKSET="nice -n -20 taskset -c 1"
+
+cd ~
+echo "#!/bin/sh
+$TASKSET ./libjxl-0.7.0/build/tools/cjxl --num_threads=1 \$@ > \$LOG_FILE 2>&1
+echo \$? > ~/test-exit-status" > jpegxl
+chmod +x jpegxl
