f071f0da8daf679d2b22abcd27a70a26896f373e
John McIver <john.mciver.iii@gmail.com>
2025-10-23 22:16:16 -0400

    [draco][local] Add v1.6.0 with bitfield statistics support

---
+++ local/draco-1.6.0/install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+tar -xf draco-1.5.6.tar.gz
+cd draco-1.5.6
+mkdir build
+cd build
+if [[ ! -z "$ALIVECC_PARALLEL_FIFO" ]]; then
+    cmake -DCMAKE_BUILD_TYPE=Release .. && \
+        "$ALIVE2_JOB_SERVER_PATH" "-j${ALIVE2_JOB_SERVER_THREADS}" make "-j${NUM_CPU_CORES}"
+elif [[ ! -z "$CLANG_PLUGIN_STATISTICS" ]]; then
+    cmake -GNinja \
+          -DCMAKE_BUILD_TYPE=Release \
+          -DCMAKE_C_FLAGS_RELEASE="$CLANG_PLUGIN_STATISTICS" \
+          -DCMAKE_CXX_FLAGS_RELEASE="$CLANG_PLUGIN_STATISTICS" \
+          .. && \
+        ninja -j${NUM_CPU_CORES} |& tee run-build.log
+else
+    cmake -DCMAKE_BUILD_TYPE=Release .. && \
+        make "-j${NUM_CPU_CORES}"
+fi
+echo $? > ~/install-exit-status
+cd ~
+unzip -o church-facade-ply.zip
+mv Church\ façade.ply draco-1.5.6/build/church.ply
+unzip -o lion-statue_ply.zip
+mv Lion\ statue_ply/Lion\ statue.ply draco-1.5.6/build/lion.ply
+TASKSET="nice -n -20 taskset -c 1"
+cd ~
+echo "#!/bin/sh
+cd draco-1.5.6/build
+$TASKSET ./draco_encoder \$@ -o out.drc -cl 10 -qp 16 > \$LOG_FILE 2>&1
+echo \$? > ~/test-exit-status" > draco
+chmod +x draco
